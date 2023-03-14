11ad008d4ebb64fc73cccc446694cdde7ff20cff
Ravindu Hiroshan <ravinduhiroshan@gmail.com>
2023-03-14 22:12:01 +0530

    BRAM create

---
+++ src/bram/hdl/bram_wrapper.v
@@ -0,0 +1,48 @@
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
+//Date        : Tue Mar 14 21:55:45 2023
+//Host        : Hiroshan-Laptop running 64-bit major release  (build 9200)
+//Command     : generate_target bram_wrapper.bd
+//Design      : bram_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module bram_wrapper
+   (BRAM_PORTA_0_addr,
+    BRAM_PORTA_0_clk,
+    BRAM_PORTA_0_din,
+    BRAM_PORTA_0_dout,
+    BRAM_PORTA_0_en,
+    BRAM_PORTA_0_rst,
+    BRAM_PORTA_0_we,
+    rsta_busy_0);
+  input [11:0]BRAM_PORTA_0_addr;
+  input BRAM_PORTA_0_clk;
+  input [7:0]BRAM_PORTA_0_din;
+  output [7:0]BRAM_PORTA_0_dout;
+  input BRAM_PORTA_0_en;
+  input BRAM_PORTA_0_rst;
+  input [0:0]BRAM_PORTA_0_we;
+  output rsta_busy_0;
+
+  wire [11:0]BRAM_PORTA_0_addr;
+  wire BRAM_PORTA_0_clk;
+  wire [7:0]BRAM_PORTA_0_din;
+  wire [7:0]BRAM_PORTA_0_dout;
+  wire BRAM_PORTA_0_en;
+  wire BRAM_PORTA_0_rst;
+  wire [0:0]BRAM_PORTA_0_we;
+  wire rsta_busy_0;
+
+  bram bram_i
+       (.BRAM_PORTA_0_addr(BRAM_PORTA_0_addr),
+        .BRAM_PORTA_0_clk(BRAM_PORTA_0_clk),
+        .BRAM_PORTA_0_din(BRAM_PORTA_0_din),
+        .BRAM_PORTA_0_dout(BRAM_PORTA_0_dout),
+        .BRAM_PORTA_0_en(BRAM_PORTA_0_en),
+        .BRAM_PORTA_0_rst(BRAM_PORTA_0_rst),
+        .BRAM_PORTA_0_we(BRAM_PORTA_0_we),
+        .rsta_busy_0(rsta_busy_0));
+endmodule
