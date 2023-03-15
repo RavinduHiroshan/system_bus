3ef75f9994592a25f652537d36f5c1df5608bd54
Ravindu Hiroshan <ravinduhiroshan@gmail.com>
2023-03-15 15:25:48 +0530

    slave port is coompleted (not tested)

--- src/bram_wrapper.v
+++ src/bram_wrapper.v
@@ -1,12 +1,13 @@
 //Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
-//Date        : Wed Mar 15 14:31:39 2023
+//Date        : Wed Mar 15 13:55:23 2023
 //Host        : Hiroshan-Laptop running 64-bit major release  (build 9200)
 //Command     : generate_target bram_wrapper.bd
 //Design      : bram_wrapper
 //Purpose     : IP block netlist
 //--------------------------------------------------------------------------------
+//Ravindu
 `timescale 1 ps / 1 ps
 
 module bram_wrapper
---
+++ src/slave.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+
+module slave(
+    input clk,
+    input reset,
+    
+	input read_en,
+	input write_en,
+
+	input master_ready,
+	input master_valid,
+	
+	output slave_valid,
+	output slave_ready,
+	input rx_address,
+	input rx_data
+	// output slave_tx_done,
+	// output rx_done,
+	// output tx_data,
+	);
+
+	wire address;
+	wire [0:7]datain;
+	wire data_ready;
+	wire data;
+	
+
+
+	slave_port slave_port(
+		.clk(clk),
+		.reset(reset),
+		
+		.read_en(read_en),
+		.write_en(write_en),
+
+		.master_ready(master_ready),
+		.master_valid(master_valid),
+		
+		.slave_valid(slave_valid),
+		.slave_ready(slave_ready),
+
+		.rx_address(rx_address),
+		.rx_data(rx_data),
+
+		.slave_tx_done(slave_tx_done),
+		.rx_done(rx_done),
+		.tx_data(tx_data),
+
+		.data_ready(data_ready),
+		.datain(datain),
+		.address(address),
+		.data(data)
+	);
+
+	bram_wrapper bram_wrapper(
+		.BRAM_PORTA_0_addr(address),
+		.BRAM_PORTA_0_clk(clk),
+		.BRAM_PORTA_0_din(datain),
+		.BRAM_PORTA_0_en(),
+		.BRAM_PORTA_0_rst(reset),
+		.BRAM_PORTA_0_we(data_ready),
+
+		.BRAM_PORTA_0_dout(data),
+		.rsta_busy_0()
+	);
+
+    
+endmodule
