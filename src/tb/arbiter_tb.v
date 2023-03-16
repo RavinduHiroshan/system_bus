ddde737f5e5ec0fd0823137a86a836d15ba6697f
Ravindu Hiroshan <ravinduhiroshan@gmail.com>
2023-03-16 11:16:47 +0530

    arbiter test bench changes

--- src/tb/arbiter_tb.v
+++ src/tb/arbiter_tb.v
@@ -2,7 +2,7 @@
 module arbiter_tb;
 parameter CLOCK_PERIOD  = 4'b1010;
 reg clk;
-reg reset,  m1_request, m2_request, m1_slave_select, m2_slave_select;
+reg reset,  m1_request, m2_request, slave_select;
 wire [1:0]bus_grant, slave_grant;
 wire m1_grant, m2_grant;
 arbiter arbiter(
@@ -10,8 +10,7 @@ arbiter arbiter(
     .reset(reset),
     .m1_request(m1_request),
     .m2_request(m2_request),
-    .m1_slave_select(m1_slave_select),
-    .m2_slave_select(m2_slave_select),
+    .slave_select(slave_select),
     .m1_grant(m1_grant),
     .m2_grant(m2_grant),
     .busy(busy),
@@ -26,65 +25,49 @@ initial begin
     end
 end
 initial begin
+    reset <= 1;
+    slave_select <= 0;
+    #(CLOCK_PERIOD*2)
     reset <= 0;
     #(CLOCK_PERIOD*2)
     m1_request <= 1;
-    reset <= 0;
     m2_request <= 0;
-    m1_slave_select <= 0;
-    m2_slave_select <= 0;
-    #(CLOCK_PERIOD*2)
-    #(CLOCK_PERIOD/2)
-    m1_slave_select <= 1;
-    m2_slave_select <= 1;
+    slave_select <= 0;
     #(CLOCK_PERIOD)
+    slave_select <= 1;
     #(CLOCK_PERIOD)
-    #(CLOCK_PERIOD)
+    slave_select <= 0;
 
-    #(CLOCK_PERIOD)
-    m1_request <= 1;
-    m2_request <= 1;
-    m1_slave_select <= 0;
-    m2_slave_select <= 0;
-    #(CLOCK_PERIOD*2)
-    #(CLOCK_PERIOD/2)
-    m1_slave_select <= 1;
-    m2_slave_select <= 1;
-    
-    #(CLOCK_PERIOD)
-    m1_request <= 0;
-    m2_request <= 1;
-    m1_slave_select <= 1;
-    m2_slave_select <= 1;
-    #(CLOCK_PERIOD*2)
-    #(CLOCK_PERIOD/2)
-    m1_slave_select <= 0;
-    m2_slave_select <= 0;
-    #(CLOCK_PERIOD)
-    #(CLOCK_PERIOD)
-
-    #(CLOCK_PERIOD)
-    m1_request <= 1;
-    m2_request <= 1;
-    m1_slave_select <= 0;
-    m2_slave_select <= 0;
-    #(CLOCK_PERIOD*2)
-    #(CLOCK_PERIOD/2)
-    m1_slave_select <= 1;
-    m2_slave_select <= 1;
-    // #30
-    // transaction_done <= 1;
-    // #30
-    // m2_request <= 1;
-    // m1_request <= 0;
-    // transaction_done <= 0;
-
-
-    // #30
     // m1_request <= 1;
+    // m2_request <= 1;
+    // m1_slave_select <= 0;
+    // m2_slave_select <= 0;
+    // #(CLOCK_PERIOD*2)
+    // #(CLOCK_PERIOD/2)
+    // m1_slave_select <= 1;
+    // m2_slave_select <= 1;
+    
+    // #(CLOCK_PERIOD)
+    // m1_request <= 0;
+    // m2_request <= 1;
+    // m1_slave_select <= 1;
+    // m2_slave_select <= 1;
+    // #(CLOCK_PERIOD*2)
+    // #(CLOCK_PERIOD/2)
+    // m1_slave_select <= 0;
+    // m2_slave_select <= 0;
+    // #(CLOCK_PERIOD)
+    // #(CLOCK_PERIOD)
 
-    // #30
-    // reset <= 1;
+    // #(CLOCK_PERIOD)
+    // m1_request <= 1;
+    // m2_request <= 1;
+    // m1_slave_select <= 0;
+    // m2_slave_select <= 0;
+    // #(CLOCK_PERIOD*2)
+    // #(CLOCK_PERIOD/2)
+    // m1_slave_select <= 1;
+    // m2_slave_select <= 1;
 
 end
 endmodule
