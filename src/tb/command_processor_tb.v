d39ff5854e58135f5edcc09a53d957c8679d366c
Ravindu Hiroshan <ravinduhiroshan@gmail.com>
2023-03-20 19:08:17 +0530

    cmd processor Co-authored-by: Udani Gamlath <udani1997@users.noreply.github.com>

--- src/tb/command_processor_tb.v
+++ src/tb/command_processor_tb.v
@@ -1,22 +1,33 @@
 `timescale 1ns / 1ps
 module command_processor_tb;
 
+parameter CLOCK_PERIOD  = 4'b1010;
+reg clk, reset ;
+reg [7:0] switch1 ;
+reg button1;
+reg button2;
+reg button3;
+
+wire [1:0]data_read_m1;
+wire [1:0]data_read_m2;
+wire [1:0]data_write;
+
 reg [7:0]data_stream  = 8'b10101010;
 reg [7:0]data_stream2 = 8'b01100010;
 
 
 command_processor command_processor(
-    input clk,
-    input reset,
+    .clk(clk),
+    .reset(reset),
 
-    input [7:0]switch1,
-    input button1,
-    input button2,
-    input button3,
+    .switch1(switch1),
+    .button1(button1),
+    .button2(button2),
+    .button3(button3),
 
-    output reg[1:0]data_read_m1,
-    output reg[1:0]data_read_m2,
-    output reg[1:0]data_write,                     
+    .data_read_m1(data_read_m1),
+    .data_read_m2(data_read_m2),
+    .data_write(data_write),                     
 
 );
 
